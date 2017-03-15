8caac6cc10a2e2945131eac1e4514fabc7bbe56d
WillySung <wanny820514@gmail.com>
2017-03-15 13:47:15 +0000

    add:RegionCheck()

--- functions.py
+++ functions.py
@@ -41,4 +41,15 @@ def get_depth():
     depth[0:479, 630:639] = depth[0:479, 620:629]
     return depth
 
+def RegionCheck(foo, ListPath):#foo defines x-coordinate of point
 
+    if (foo <= 130) and (ListPath[0] is not 0):
+        ListPath[0] = 0
+    if (foo > 130) and (foo <= 320) and (ListPath[1] is not 0):
+        ListPath[1] = 0
+    if (foo > 320) and (foo <= 510) and (ListPath[2] is not 0):
+        ListPath[2] = 0
+    if (foo > 510) and (ListPath[3] is not 0):
+        ListPath[3] = 0
+
+    return ListPath
